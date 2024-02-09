65de14dd2604b7ed491a63ec96671f91a95d2a93
DarioLoForti <lofortidario@gmail.com>
2024-02-09 16:10:22 +0100

    stampo a schermo hello world passato dinamicamente tramite variabile

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -14,7 +14,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12 text-center mt-5">
-                <h1>Hello World</h1>
+                <h1>{{ $welcome }}</h1>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('homepage');
+    $welcome = 'Hello World';
+    return view('homepage', compact('welcome'));
 });
